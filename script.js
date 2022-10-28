d6cf765986d50b4b2dd8cf30f0b404e4c6867cc0
nazmul7b7 <nazmul7b7@gmail.com>
2022-10-28 08:15:09 -0400

    feat: add a function to play a single round of game

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function getComputerChoice() {
 
 //console.log(getComputerChoice());
 
+function playRound(playerSelection, computerSelection) {
+    //const playerChoice = playerSelection.toLowerCase();    
+    if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return "You Win! rock beats paper";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You Win! paper beats rock";
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return "You Win! scissor beats paper";
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerChoice = "rock";
+const playerSelection = playerChoice.toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
