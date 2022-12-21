dc77d4aa74410eade4a43d6f861b7601ad8631d8
nazmul7b7 <nazmul7b7@gmail.com>
2022-12-21 14:15:28 +0600

    format files with prettier

--- script.js
+++ script.js
@@ -2,36 +2,37 @@ const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissor = document.querySelector(".scissor");
 const divAnswers = document.querySelector(".answer");
-let computerCount = 0, humanCount = 0, ties = 0;
-
+let computerCount = 0,
+  humanCount = 0,
+  ties = 0;
 
 rock.addEventListener("click", () => {
-    console.log(game("rock"));
-  });
+  console.log(game("rock"));
+});
 
 paper.addEventListener("click", () => {
-    console.log(game("paper"));
+  console.log(game("paper"));
 });
 
 scissor.addEventListener("click", () => {
-    console.log(game("scissor"));
+  console.log(game("scissor"));
 });
 
 // Picks the computer's choice randomly.
 function getComputerChoice() {
-    const options = ['rock', 'paper', 'scissor'];
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
+  const options = ["rock", "paper", "scissor"];
+  const choice = options[Math.floor(Math.random() * options.length)];
+  return choice;
 }
 
 const updateUI = (compSelection, humanSelection, result) => {
-    let textContent = "";
+  let textContent = "";
 
-    if (result === "Machine") computerCount++;
-    else if (result === "Player") humanCount++;
-    else ++ties;
+  if (result === "Machine") computerCount++;
+  else if (result === "Player") humanCount++;
+  else ++ties;
 
-    textContent = `
+  textContent = `
     <p>Computer: ${compSelection}</p>
     <p>Human: ${humanSelection}</p>
     <p>Winner: ${result}</p>
@@ -40,42 +41,40 @@ const updateUI = (compSelection, humanSelection, result) => {
     <p>Ties in Game: ${ties}</p>
   `;
 
+  divAnswers.innerHTML = textContent;
 
-    divAnswers.innerHTML = textContent;
-
-    if (humanCount >= 5 || computerCount >= 5) {
-        if (humanCount > computerCount) {
-            alert("Won by human");
-        } else {
-            alert("Won by machine");
-        }              
+  if (humanCount >= 5 || computerCount >= 5) {
+    if (humanCount > computerCount) {
+      alert("Won by human");
+    } else {
+      alert("Won by machine");
     }
+  }
 };
 
-
 function playRound(playerSelection, computerSelection) {
-    let outcome;        
-    if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        outcome = "Player";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        outcome = "Player";
-    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-        outcome = "Player";
-    } else if (playerSelection === computerSelection) {
-        outcome = "Tie";
-    } else {
-        outcome = "Machine";
-    }
+  let outcome;
+  if (playerSelection === "rock" && computerSelection === "scissor") {
+    outcome = "Player";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    outcome = "Player";
+  } else if (playerSelection === "scissor" && computerSelection === "paper") {
+    outcome = "Player";
+  } else if (playerSelection === computerSelection) {
+    outcome = "Tie";
+  } else {
+    outcome = "Machine";
+  }
 
-    updateUI(computerSelection, playerSelection, outcome);
+  updateUI(computerSelection, playerSelection, outcome);
 
-    return outcome;
+  return outcome;
 }
 
 const game = (playerChoice) => {
-    const computerSelection = getComputerChoice();
-    const winner = playRound(playerChoice, computerSelection);
-    if (winner === "Player") return "Player won";
-    else if (winner === "Machine") return "Machine won";
-    else return "Tie";
+  const computerSelection = getComputerChoice();
+  const winner = playRound(playerChoice, computerSelection);
+  if (winner === "Player") return "Player won";
+  else if (winner === "Machine") return "Machine won";
+  else return "Tie";
 };
